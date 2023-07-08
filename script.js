aecfd79883d4d693620e9b596c2f2109f0dd7862
vinfreecs <89305950+vinfreecs@users.noreply.github.com~>
2023-07-08 20:52:49 +0530

    almost all functionalities are done color picker is remaining

--- script.js
+++ script.js
@@ -1,18 +1,62 @@
 const grid = document.querySelector(".grid");
 const child = document.createElement("div");
-let pixels = document.getElementById("pixels")
-let flexBasisValue = 640 / 64;
-child.style.flexBasis = `${flexBasisValue}px`;
-for (let i = 0; i < 64 * 64; i++) {
-  grid.appendChild(child.cloneNode(true));
-}
-const gridChildren = document.querySelectorAll(".grid div");
-gridChildren.forEach((gridChild) => {
-  gridChild.addEventListener(
-    "mouseover",
-    (e) => {
-      e.target.style.backgroundColor = "red";
-    },
-    false
-  );
+let pixels = document.getElementById("pixels");
+let drawing = false;
+pixels.addEventListener("input", (event) => {
+  pixels.value = event.target.value;
+  let pixelValue = event.target.value;
+  const label = document.querySelector("label");
+  label.textContent = `pixels : ${event.target.value}`;
+  etch(pixelValue);
 });
+function etch(pix = pixels.value) {
+  grid.querySelectorAll("*").forEach((kid) => {
+    kid.remove();
+  });
+  let flexBasisValue = 640 / pix;
+  child.style.flexBasis = `${flexBasisValue}px`;
+  for (let i = 0; i < pix * pix; i++) {
+    grid.appendChild(child.cloneNode(true));
+  }
+  const gridChildren = document.querySelectorAll(".grid div");
+  grid.addEventListener("mousedown", () => {
+    drawing = true;
+    console.log(drawing);
+  });
+  gridChildren.forEach((gridChild) => {
+    gridChild.addEventListener(
+      "mouseover",
+      (e) => {
+        if (drawing) {
+          e.target.style.backgroundColor = "red";
+        }
+      },
+      false
+    );
+  });
+  grid.addEventListener("mouseup", () => {
+    drawing = false;
+  });
+}
+const cleanButton = document.querySelector(".clean");
+cleanButton.addEventListener("click", () => {
+  grid.addEventListener("mousedown", () => {
+    drawing = true;
+  });
+  const gridChildren = document.querySelectorAll(".grid div");
+  gridChildren.forEach((gridChild) => {
+    gridChild.addEventListener(
+      "mouseover",
+      (e) => {
+        if (drawing) {
+          e.target.style.backgroundColor = "";
+        }
+      },
+      false
+    );
+  });
+  grid.addEventListener("mouseup", () => {
+    drawing = false;
+  });
+});
+etch();
